674a025ce5436ba9a034941b4706aef7351e5abf
ira <rozira@optonline.net>
2015-02-18 18:34:51 -0500

    initial commit

---
+++ Mobile.Web/js/configuration.js
@@ -0,0 +1,16 @@
+﻿MovieTickets.configuration = (function () {
+
+    //var serviceUrl = "http://dva-1l8gfs1-l/movietickets.webapi/api/";
+    var serviceUrl = "http://api.kendomobilebook.com/api/";
+    
+
+    return {            
+        serviceUrl: serviceUrl,
+        accountUrl: serviceUrl + "Account/",
+        getMovieListUrl: serviceUrl + "Movies/GetMovieList/",        
+        getTheaterListForMovieUrl: serviceUrl +
+            "Theater/Get/",
+        getTrailersUrl: serviceUrl + "Movies/GetTrailers/",
+        purchaseTicketsUrl: serviceUrl + "Tickets"
+    }
+})();
